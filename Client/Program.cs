e952c2697ede06ea1361a3b7ad6cc11acaca578a
VladBod <34280879+JesusCristi@users.noreply.github.com>
2020-04-14 15:40:59 +0300

    asdfasd

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Host = "localhost";
+            const int Port = 16842;
+            var channel = new Channel($"{Host}:{Port}", ChannelCredentials.Insecure);
+            Console.WriteLine("Enter your name please -> ");
+            do
+            {
+                var key = Console.ReadLine();
+                var client = new Generated.NameRequestService.NameRequestServiceClient(channel);
+                client.OutputName(new Generated.Name
+                {
+                    Name_ = key
+                });
+            } while (true);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
